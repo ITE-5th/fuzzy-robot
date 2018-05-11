9fa6a52cadb29fd7a0a5607086a4f7e7a0bbb47c
Obada Jabassini <obada.jabassini@gmail.com>
2018-05-11 18:08:07 +0300

    Solve problems concurrently

--- solver/multi_objective_optimization_solver.py
+++ solver/multi_objective_optimization_solver.py
@@ -1,22 +1,29 @@
+from multiprocessing.pool import Pool
+
 from platypus import Problem, Subset, NSGAII
 
 from solver.solver import Solver
 
 
 class MultiObjectiveOptimizationSolver(Solver):
-    def __init__(self, output_u, output_w):
+    def __init__(self, output_u, output_w, iterations=10000):
         super().__init__()
         self.u_universe, self.w_universe = output_u.universe, output_w.universe
+        self.iterations = iterations
 
     def solve(self):
         u_problem, w_problem = self.build_problems()
-        algorithm = NSGAII(u_problem)
-        algorithm.run(1000)
-        u = algorithm.result[0].objectives
-        algorithm = NSGAII(w_problem)
-        algorithm.run(1000)
-        w = algorithm.result[0].objectives
+        with Pool(2) as p:
+            u, w = p.map(self._solve, [(u_problem, self.iterations), (w_problem, self.iterations)])
+            p.join()
         return u, w
+
+    @staticmethod
+    def _solve(args):
+        problem, iterations = args
+        algorithm = NSGAII(problem)
+        algorithm.run(iterations)
+        return algorithm.result[0].objectives
 
     def u_function(self, x):
         x = x[0][0]
@@ -30,7 +37,6 @@ class MultiObjectiveOptimizationSolver(Solver):
         u_problem, w_problem = Problem(1, 3), Problem(1, 3)
         u_problem.types[:] = Subset(self.u_universe, 1)
         w_problem.types[:] = Subset(self.w_universe, 1)
-        u_problem.directions[:] = Problem.MAXIMIZE
-        w_problem.directions[:] = Problem.MAXIMIZE
+        u_problem.directions[:] = w_problem.directions[:] = Problem.MAXIMIZE
         u_problem.function, w_problem.function = self.u_function, self.w_function
         return u_problem, w_problem
