75094315daef70e945c0071110ecfad7edd38269
Obada Jabassini <obada.jabassini@gmail.com>
2018-05-17 08:46:37 +0300

    Add server

--- fuzzy_controller/fuzzy_controller.py
+++ fuzzy_controller/fuzzy_controller.py
@@ -8,8 +8,9 @@ class FuzzyController(metaclass=ABCMeta):
     def __init__(self, input_dl, input_df, input_dr, input_a, input_p, input_ed, output_u, output_w):
         self.input_dl, self.input_df, self.input_dr, self.input_a, self.input_p, self.input_ed = input_dl, input_df, input_dr, input_a, input_p, input_ed
         self.output_u, self.output_w = output_u, output_w
-        self.controller = ctrl.ControlSystem(self.build_rules())
-        self.controller = ctrl.ControlSystemSimulation(self.controller)
+        rules = self.build_rules()
+        controller = ctrl.ControlSystem(rules)
+        self.controller = ctrl.ControlSystemSimulation(controller)
 
     @abstractmethod
     def build_rules(self):
