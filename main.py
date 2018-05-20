9bb50aa20f318976674e49f125fe30f227bed696
ZaherAirout <m.zaher.airout@gmail.com>
2018-05-20 11:24:11 +0300

    reformat code

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from math import sqrt, atan2, radians, cos, sin, hypot
+from math import sqrt, atan2, cos, sin, hypot
 
 from fuzzy_controller.fuzzy_system import FuzzySystem
 
-goal_threshold = 1
+goal_threshold = 0.5
 
 
 def success(xo, yo, xd, yd):
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     x, y, theta = 0, 0, 0
 
     # target position and orientation
-    x_d, y_d, theta_d = 2, 0, 0
+    x_d, y_d, theta_d = 2, 2, 0
 
     # wheel diameter
     R = 0.085
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     dl = 4
     df = 4
     dr = 4
-    a = max(min(a, 4), -4)
+    # a = max(min(a, 4), -4)
     p = max(min(p, 20), 0)
     ed = max(min(ed, 1), -1)
     print(f"dl:{dl}, df:{df}, dr:{dr}, a:{a}, p:{p}, ed:{ed}")
@@ -58,21 +58,20 @@ if __name__ == '__main__':
     # a = msg['alpha']
     # p = msg['p']
     # ed = msg['ed']
+    degree = 10
     goal_reached = success(x, y, x_d, y_d)
     while not goal_reached:
         u, w = fuzzy_system.run(dl, df, dr, a, p, ed)
         theta += w
-        theta = theta % radians(360)
+        x += u * cos(theta)
+        y += u * sin(theta)
 
-        x = u * cos(theta)
-        y = u * sin(theta)
-
-        a = round(atan2(y_d - y, x_d - x) - theta, 2)
-        p_current = sqrt(pow(x_d - x, 2) + pow(y_d - y, 2))
-        ed = round(p_current - p, 2)
-        p = round(p_current, 2)
-        print(f"u = {round(u,2)}, w = {round(w,2)}")
-        print(f"position is : x:{round(x,2)},y:{round(y,2)},theta:{round(theta,2)}")
+        a = round(atan2(y_d - y, x_d - x) - theta, degree)
+        p_current = hypot(x_d - x, y_d - y)
+        ed = round(p_current - p, degree)
+        p = round(p_current, degree)
+        print(f"u = {round(u,degree)}, w = {round(w,degree)}")
+        print(f"position is : x:{round(x,degree)},y:{round(y,degree)}, theta = {round(theta, degree)},alpha:{round(a,degree)}")
         goal_reached = success(x, y, x_d, y_d)
     if goal_reached:
         print('GOAL REACHED')
