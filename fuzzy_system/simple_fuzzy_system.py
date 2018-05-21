5b6847d0f23077ff62f1148a9fa3924d0dca2f0e
anjd <ite.anjd@outlook.com>
2018-05-21 09:51:45 +0300

    simple terms

--- fuzzy_system/simple_fuzzy_system.py
+++ fuzzy_system/simple_fuzzy_system.py
@@ -1,5 +1,6 @@
 import numpy as np
 import skfuzzy.control as ctrl
+from skfuzzy import fuzzy_or, trimf, pimf
 
 from fuzzy_system.fuzzy_system import FuzzySystem
 
@@ -44,12 +45,29 @@ class SimpleFuzzySystem(FuzzySystem):
         ]
 
     def build_inputs(self):
-        min_distance, max_distance = 10, 140
+        # min_distance, max_distance = 10, 140
+        min_distance, max_distance = 0, 70
         min_velocity, max_velocity = 0, 100
         input_front = ctrl.Antecedent(np.arange(min_distance, max_distance, self.step), 'input_front')
         input_left = ctrl.Antecedent(np.arange(min_distance, max_distance, self.step), 'input_left')
         input_right = ctrl.Antecedent(np.arange(min_distance, max_distance, self.step), 'input_right')
         input_velocity = ctrl.Antecedent(np.arange(min_velocity, max_velocity, self.step), 'input_velocity')
+
+        input_front = self.tri_pi_tri(input_front, "close", [0, 0, 30, 40])
+        input_front = self.tri_pi_tri(input_front, "far", [30, 40, 70, 70])
+
+        input_left = self.tri_pi_tri(input_left, "close", [0, 0, 30, 40])
+        input_left = self.tri_pi_tri(input_left, "far", [30, 40, 70, 70])
+
+        input_right = self.tri_pi_tri(input_right, "close", [0, 0, 30, 40])
+        input_right = self.tri_pi_tri(input_right, "far", [30, 40, 70, 70])
+
+        input_velocity = self.tri_pi_tri(input_velocity, "slow", [0, 0, 5, 15])
+        input_velocity = self.tri_pi_tri(input_velocity, "medium", [10, 20, 30, 40])
+        input_velocity = self.tri_pi_tri(input_velocity, "fast", [35, 40, 50, 55])
+        input_velocity = self.tri_pi_tri(input_velocity, "very_fast", [50, 60, 70, 80])
+        input_velocity = self.tri_pi_tri(input_velocity, "stop", [70, 85, 100, 100])
+
         return input_front, input_left, input_right, input_velocity
 
     def build_outputs(self):
@@ -57,4 +75,29 @@ class SimpleFuzzySystem(FuzzySystem):
         theta = 180
         output_velocity = ctrl.Consequent(np.arange(min_velocity, max_velocity, self.step), "output_velocity")
         output_angle = ctrl.Consequent(np.arange(-theta, theta, self.step), "output_angle")
+
+        output_velocity = self.tri_pi_tri(output_velocity, "slow", [0, 0, 5, 15])
+        output_velocity = self.tri_pi_tri(output_velocity, "medium", [10, 20, 30, 40])
+        output_velocity = self.tri_pi_tri(output_velocity, "fast", [35, 40, 50, 55])
+        output_velocity = self.tri_pi_tri(output_velocity, "very_fast", [50, 60, 70, 80])
+        output_velocity = self.tri_pi_tri(output_velocity, "stop", [70, 85, 100, 100])
+
+        output_angle["left"] = trimf(output_angle.universe, [-90, -90, 25])
+        output_angle["right"] = trimf(output_angle.universe, [-25, 0, 25])
+        output_angle["front"] = trimf(output_angle.universe, [-25, 90, 90])
+
         return output_velocity, output_angle
+
+    @staticmethod
+    def tri_pi_tri(var, term: str, values):
+        var[term] = fuzzy_or(
+            var.universe, trimf(var.universe, [values[0], values[1], values[1]]),
+            var.universe, pimf(var.universe, values[1], values[1], values[2], values[2])
+        )[1]
+
+        var[term] = fuzzy_or(
+            var.universe, var[term].mf,
+            var.universe, trimf(var.universe, [values[2], values[2], values[3]])
+        )[1]
+
+        return var
